0153057584bc16101c83983e7233b768a6a33ca4
Jeremy-wong123 <1533393@learn.vsb.bc.ca>
2025-09-23 22:00:26 +0000

    dfuyasdygegfjkeg

--- 2.3/coding3.py
+++ 2.3/coding3.py
@@ -25,3 +25,17 @@ Would you like fries for $3? (Yes/No)
 > yes
 Your total is $3.42
 """
+bur = input(" Would you like a burger for 5$?").lower().strip()
+fri = input("Would you like fries for 3$?").lower().strip()
+cost = 0
+if(bur == 'yes' and fri == 'no'):
+    cost = cost + 5 * 1.14
+    print("Your total is $" + str(cost))
+elif(bur == 'yes' and fri == 'yes'):
+    cost = cost + (5 + 3) * 1.14
+    print("Your total is $" + str(cost))
+elif(bur == 'no' and fri == 'yes'):
+    cost = cost + 3 * 1.14
+    print("Your total is $" + str(cost))
+else:
+    print("Your total is $0")
