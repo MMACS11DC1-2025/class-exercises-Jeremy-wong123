d2faa08d23949463f83f736cfa922b974cfa177b
Jeremy-wong123 <1533393@learn.vsb.bc.ca>
2025-09-23 21:50:38 +0000

    yiopwdhfkasdfkadfl

--- 2.3/coding2.py
+++ 2.3/coding2.py
@@ -14,3 +14,14 @@ Judge 4: 8.5
 Judge 5: 9
 Your Olympic score is 8.0
 """
+import random
+print("Your score is from 0-10, half marks are allowed")
+ra = input(" What is Judge 1 score?(0-10)")
+rt = input(" What is Judge 2 score?(0-10)")
+re = input(" What is Judge 3 score?(0-10)")
+ri = input(" What is Judge 4 score?(0-10)")
+rn = input(" What is Judge 5 score?(0-10)")
+
+avg = (int(ra) + int(rt) + int(re) + int(ri) + int(rn))/5
+
+print("Your average score is: " + str(avg))
