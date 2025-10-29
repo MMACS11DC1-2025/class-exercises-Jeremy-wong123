14d5a609c0f3dfb280223f99df43325e782359d4
Jeremy-wong123 <1533393@learn.vsb.bc.ca>
2025-10-29 21:47:21 +0000

    dfgdfgfdg

--- 4.4/main.py
+++ 4.4/main.py
@@ -7,15 +7,16 @@ def snow(x, y):
   xcoor = 0
   ycoor = 0
   if y > 0:
-    for i in range(6):
-      xcoor = random.choice(range(200))
-      ycoor = random.choice(range(200))
-      t.forward(y)
-      t.right(60)
-      t.color('blue')
-      t.stamp()
-      t.penup()
+    for i in range(200):
       t.goto(xcoor, ycoor)
+      for i in range(6):
+        xcoor = random.choice(range(200))
+        ycoor = random.choice(range(200))
+        t.forward(y)
+        t.right(60)
+        t.color('blue')
+        t.stamp()
+        t.penup()
     snow(x/0.5, y/0.5)
   else:
     t.color('blue')
