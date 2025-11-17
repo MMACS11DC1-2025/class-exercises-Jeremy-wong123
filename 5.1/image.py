a76600f115dc3e72e8e592aed7bf4b54f57f51db
Jeremy-wong123 <1533393@learn.vsb.bc.ca>
2025-11-17 21:15:49 +0000

    xvcxbvc

--- 5.1/image.py
+++ 5.1/image.py
@@ -1,1 +1,10 @@
-from PIL import Image
+
+
+def is_green(r,g,b):
+    return 0 <- 25 and 230 < g <= 255 and 0 <= b < 25
+
+def color(r, g, b):
+    if r >= 0 and r < 25 and g > 230 and g <= 255 and b >= 0 and b < 25:
+        return 'green'
+    else:
+        return 'other'
--- 5.1/image_open.py
+++ 5.1/image_open.py
@@ -1,51 +1,22 @@
 from PIL import Image
+import image
 
 image_green = Image.open("5.1/kid-green.jpg").load()
 image_beach = Image.open("5.1/beach.jpg").load()
 
 w = image_beach.width
 h = image_beach.height
-
-def is_green(r, g, b):
-    if r >= 0 and r < 25 and g > 230 and g <= 255 and b >= 0 and b < 25:
-        return True
-    else:
-        return False
-print(image_green[0,0])
-
-def color(r, g, b):
-    hal = []
-    for i in range(3):
-        col = int(r)
-        hal.append(col)
-    return hal
-
-pixel = image_green[0, 0]
-r = pixel[0]
-g = image_green[0,0][1]
-b = image_green[0,0][2]
-
-#print(is_green(r, g, b))
-#print(color(r, g, b))
-
+image_output = Image.open("kid-green.jpg")
 def bw(w, h):
     for i in range(w):
         for b in range(h):
-            r = image_beach[i, b][0]
-            g = image_beach[i, b][1]
-            bl = image_beach[i, b][2]
-            if r >= 128:
-                r = 255
-            else: 
-                r = 0
-            if g >= 128:
-                g = 255
-            else:
-                g = 0
-            if b >= 128:
-                b = 255
-            else: 
-                b = 0
-            image_output.putpixel()
+            r = image_green[i, b][0]
+            g = image_green[i, b][1]
+            bl = image_green[i, b][2]
+            
+            if image.is_green(r,g,bl):
+                beach_colour = image_beach[i, b]
+                image_output.putpixel((i,b), beach_colour) 
+    image_output.save("output.png", "png")
         
         
