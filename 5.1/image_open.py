a68ee67b3946cfe384691cdc29d72ca38f282944
Jeremy-wong123 <1533393@learn.vsb.bc.ca>
2025-11-19 21:08:11 +0000

    jajajaja

--- 5.1/image_open.py
+++ 5.1/image_open.py
@@ -3,20 +3,20 @@ import image
 
 image_green = Image.open("5.1/kid-green.jpg").load()
 image_beach = Image.open("5.1/beach.jpg").load()
+image_output = Image.open("5.1/kid-green.jpg")
+w = image_output.width
+h = image_output.height
 
-w = image_beach.width
-h = image_beach.height
-image_output = Image.open("kid-green.jpg")
-def bw(w, h):
-    for i in range(w):
-        for b in range(h):
-            r = image_green[i, b][0]
-            g = image_green[i, b][1]
-            bl = image_green[i, b][2]
+for i in range(w):
+    for b in range(h):
+        r = image_green[i, b][0]
+        g = image_green[i, b][1]
+        bl = image_green[i, b][2]
             
-            if image.is_green(r,g,bl):
-                beach_colour = image_beach[i, b]
-                image_output.putpixel((i,b), beach_colour) 
-    image_output.save("output.png", "png")
+        if image.is_green(r, g, bl):
+            beach_colour = image_beach[i, b]
+            xy = (i, b)
+            image_output.putpixel(xy, beach_colour) 
+image_output.save("output.png", "png")
         
         
