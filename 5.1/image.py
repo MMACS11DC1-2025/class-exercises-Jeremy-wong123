760573e39c7222d952be65e1c27283d753664e2c
Jeremy-wong123 <1533393@learn.vsb.bc.ca>
2025-11-19 21:12:51 +0000

    vgfdg

--- 5.1/image.py
+++ 5.1/image.py
@@ -1,7 +1,7 @@
 
 
 def is_green(r,g,b):
-    return r <- 25 and 230 < g <= 255 and 0 <= b < 25
+    return r <= 25 and 230 < g <= 255 and 0 <= b < 25
 
 def color(r, g, b):
     if r >= 0 and r < 25 and g > 230 and g <= 255 and b >= 0 and b < 25:
