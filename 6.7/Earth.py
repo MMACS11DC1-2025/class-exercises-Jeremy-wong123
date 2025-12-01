0b607f6496f841e27805050ec36ec2cb761ea493
Jeremy-wong123 <1533393@learn.vsb.bc.ca>
2025-12-01 21:00:05 +0000

    eghfgh

---
+++ 6.7/Earth.py
@@ -0,0 +1,15 @@
+'''
+PROJECT IDEAS
+
+1. A program that analyzes a set of planets
+    - Analyzes and compares it to Earth
+    - Judges based on color(blue = ocean, white= clouds, green/red/yellow = land)
+    - Uses a breef description of the planet
+        (if the planet is classified as a gas giant land identification is exempt)
+    -outputs a ranking of the 10 planets including a score out of 5
+
+2. A program that analyzes different scenery or views 
+    -From the scenery and sky patterns it will try to guess the time period
+    (Morning, noon, midnight, midday, sunrise, sunset)
+    -Output is the predicted time including a useful tip and a time range 
+
