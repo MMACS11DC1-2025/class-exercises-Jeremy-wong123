040f4c3f36e7c9c863adf2587df677fb1f45a3f7
Jeremy-wong123 <1533393@learn.vsb.bc.ca>
2025-12-03 21:34:35 +0000

    vzv

--- 6.2/search.py
+++ 6.2/search.py
@@ -2,7 +2,8 @@ file = open("6.2/spotify.csv")
 junk = file.readline()
 
 drake_data = []
-
+data = []
+song = []
 for line in file:
 	items = line.strip().split(",")
 	artist = str(items[-1])
@@ -11,7 +12,23 @@ for line in file:
 	
 	if artist == "Drake":
 		drake_data.append([danceability, songtitle, artist])
+		data.append(danceability)
+		song.append(songtitle)
 
+
+for i in range(len(data)):
+	current = data[-1]
+	index = i
+
+	for j in range(i-1, len(data)):
+		if data[j] > current:
+			current = data[j]
+			index = j
+	
+	data[index], data[i] = data[i],
+	data[index]
+
+top5 = data[:5]
 print("Dance score \tSong")
-for item in drake_data:
-	print(str(item[0]) + "\t\t" + item[1] + " by " + item[2])
+for item in top5:
+	print(str(item))
