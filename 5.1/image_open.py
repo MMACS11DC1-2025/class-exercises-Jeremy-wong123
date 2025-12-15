93be938d1e937f702af049893272664a9acdde82
Jeremy-wong123 <1533393@learn.vsb.bc.ca>
2025-12-15 01:57:11 +0000

    sadsd

--- 5.1/image_open.py
+++ 5.1/image_open.py
@@ -20,3 +20,46 @@ for i in range(w):
 image_output.save("output.png", "png")
         
         
+'''
+for i in range(w):
+    for b in range(h):
+        r = image_green[i,b][0]
+        g = image_green[i,b][1]
+        b = image_green[i,b][2]
+        if image.is_green(r, g, b):
+        beachcolor = image_beach[i, b]
+        xy = (i, b)
+        image_output.putpixel(xy, beach _colour)
+    image_output.save("output.png", "png")
+
+
+
+
+for x in range(width):
+    for y in range(height):
+        r = jb[x,y][0]
+        g = jb[x,y][1]
+        b = jb[x,y][2]
+
+        if image.yellow(r, g, b) == 'yellow':
+            newpixel = (255, 255, 255)
+            xy = (x,y)
+            file.putpixel(xy, newpixel)
+
+file.save('output.png', "png")
+
+for x in range(width):
+    for y in range(height):
+        r = jb[x, y][0]
+        g = jb[x, y][1]
+        b = jb[x, y][2]
+            
+        if image.is_light(r, g, b):
+            newp = (255, 255, 255)
+            xy = (x, y)
+        else:
+            newp = (0, 0, 0)
+            xy = (x, y)
+        file.putpixel(xy, newp) 
+file.save("output.png", "png")
+'''
--- 6.7/Sun.py
+++ 6.7/Sun.py
@@ -57,9 +57,19 @@ for a in range(len(stars)):
     stardata.append(startype)
     secondstardata.append(functions.starclass(startype[0]))
     tend = time.time()
-print(stardata)
-print('\n')
-print(secondstardata)
+
+    output =  '''
+    name:{} 
+    time: {:.2f}
+    colour: {}
+    colour percentage: {:.2f}
+    similarity to sun: {}
+    class: {}
+    Temp: {}
+    {}
+    \n
+    '''.format(names[a], tend-tstar, stardata[a][0], stardata[a][0], 'n/a', secondstardata[a][0], secondstardata[a][1], secondstardata[a][2])
+    print(output)
 
 '''
 
